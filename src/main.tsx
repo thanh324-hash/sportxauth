c34016b85448d2485b2d6a1829f681f9dad3e1da
hoangthanhk4416-cell <hoangthanhk4416@gmail.com>
2026-07-30 01:29:22 +0700

    fix: remove mobile workspace offset

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './styles.css'
 import './auth.css'
 import './connections.css'
 import './ai.css'
 import './media.css'
+import App from './App'
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
